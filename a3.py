652c2e23b5abc378f34a5c00c24e471df2e4512e
Microwen <maoyouwengo@gmail.com>
2016-10-08 17:38:06 +1000

    t1 t2 class defined

--- a3.py
+++ a3.py
@@ -43,6 +43,8 @@ class SimpleTileApp(object):
 
         # Add your code here
 
+
+
     def _handle_swap(self, from_pos, to_pos):
         """
         Run when a swap on the grid happens.
@@ -55,10 +57,90 @@ class SimpleTileApp(object):
         """
         print("SimplePlayer scored {}!".format(score))
 
+class SimplePlayer(object):
+        def __init__(self):
+            self._score = 0
+            self._swap = 0
+        def add_score(self, score):
+            self._score += score
+            return self._score
+        def get_score(self):
+            return self._score
+        def reset_score(self):
+            self._score = 0
+        def record_swap(self):
+            self._swap += 1
+            return self._swap
+        def get_swaps(self):
+            return self._swap
+        def reset_swaps(self):
+            self._swap = 0
+
+class SimpleStatusBar(tk.Frame):
+    def __init__(self, master):
+        self._master = master
+        self._player = SimplePlayer()
+
+    def get_player():
+        return self._player
+
+class Character(object):
+    def __init__(self,max_health):
+        pass
+
+    def get_max_health(self):
+        pass
+
+    def get_health(self):
+        pass
+
+    def lose_health(self, amount):
+        pass
+
+    def gain_health(self, amount):
+        pass
+
+    def reset_health(self):
+        pass
+
+class Enemy(Character):
+    def __init__(self, type, max_health, attack):
+        pass
+
+    def get_type(self):
+        pass
+
+    def attack(self):
+        pass
+
+class Player(Character):
+    def __init__(self, max_health, swaps_per_turn):
+        pass
+
+    def record_swap(self):
+        pass
+
+    def get_swaps(self):
+        pass
+
+    def reset_swaps(self):
+        pass
+
+    def attack(self, runs):
+        pass
+
+class VersusStatusBar(object):
+    pass
+
+class ImageTileGridView(object):
+    pass
+
+class SinglePlayerTileApp(object):
+    pass
+
 def task1():
     # Add task 1 GUI code here
     pass
-
 def task2():
     # Add task 2 GUI code here
     pass
